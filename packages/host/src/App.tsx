bc801d5d485fb5b2aa70efd7c8e2e6b309a95d93
fedeviotti <fedeviotti@gmail.com>
2021-11-18 18:24:49 +0100

    Feat: update projects names

--- packages/host/src/App.tsx
+++ packages/host/src/App.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import {Button} from "react-ts-lib-fv";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Button } from '@fedeviotti/library';
 
-import "./index.scss";
+import './index.scss';
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
@@ -10,7 +10,11 @@ const App = () => (
     <div>Framework: react</div>
     <div>Language: TypeScript</div>
     <div>CSS: Tailwind</div>
-      <Button type="primary" text="A Button" onClick={() => window.console.log('Click')} />
+    <Button
+      type="primary"
+      text="A Button"
+      onClick={() => window.console.log('Click')}
+    />
   </div>
 );
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
