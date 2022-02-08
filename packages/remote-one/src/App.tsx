f0e9b29805b05af4a28398964d088afb877dec0e
fedeviotti <fedeviotti@gmail.com>
2022-02-08 10:57:23 +0100

    Feat: Add library to remote

--- packages/remote-one/src/App.tsx
+++ packages/remote-one/src/App.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-import "./index.scss";
+import './index.scss';
+import { ButtonLib } from '@fedeviotti/library';
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
@@ -9,6 +10,11 @@ const App = () => (
     <div>Framework: react</div>
     <div>Language: TypeScript</div>
     <div>CSS: Tailwind</div>
+    <ButtonLib
+      type="primary"
+      text="My second button"
+      onClick={() => window.console.log('Click')}
+    />
   </div>
 );
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
